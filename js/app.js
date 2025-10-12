0a6f46fd43c14b34952f98fcd15d34932c76e9e5
Cyril Krylatov <cyril.krylatov+github@gmail.com>
2025-10-12 21:31:36 +0200

    Moving files into assets/

--- js/app.js
+++
@@ -1,8 +0,0 @@
-const TEMPLATE = document.querySelector('#template')
-const ITEMS = 10
-let ITERATOR = 0
-
-for (ITERATOR; ITERATOR < ITEMS; ITERATOR++) {
-    const TEMPLATE_CONTENT = TEMPLATE.content.cloneNode(true)
-    document.body.append(TEMPLATE_CONTENT)
-}
