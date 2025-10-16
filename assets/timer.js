4035130736602eb5376ce438233171348a47763f
Cyril Krylatov <cyril.krylatov+github@gmail.com>
2025-10-16 21:03:01 +0200

    Lance les timers si le timer principal est lancé

--- assets/timer.js
+++ assets/timer.js
@@ -1,5 +1,59 @@
+const FORMATTER = (duration) => new Intl.DurationFormat('fr', { style: 'digital' }).format(duration)
+
 export class Timer {
+  #element
+  #toggleTimerElement
+  #displayTimerElement
+  #duration = { hours: 0, minutes: 0, seconds: 0 }
+  #intervalId = 0
+
+  /**
+   * @param {Element} element
+   */
   constructor (element) {
-    console.log(element)
+    this.#element = element
+    this.#element.id = `cell-${window.crypto.randomUUID()}`
+    this.#setElements()
+    this.#setEvents()
+  }
+
+  #setElements () {
+    this.#toggleTimerElement = this.#element.querySelector('.js-playeronfield')
+    this.#displayTimerElement = this.#element.querySelector('.js-timer')
+  }
+
+  #setEvents () {
+    this.#toggleTimerElement.addEventListener('change', this.#toggle.bind(this))
+  }
+
+  /**
+   * @param {Boolean} checked
+   */
+  #toggle ({ target: { checked } }) {
+    if (window.is_timer_launched === false) {
+      return
+    }
+    this.launch()
+  }
+
+  launch () {
+    clearInterval(this.#intervalId)
+    if (this.#toggleTimerElement.checked === false) {
+      return
+    }
+    this.#intervalId = setInterval(this.#updateTimer.bind(this), 1000)
+  }
+
+  #updateTimer () {
+    this.#duration.seconds++
+    if (this.#duration.seconds > 59) {
+      this.#duration.minutes++
+      this.#duration.seconds = 0
+    }
+    if (this.#duration.minutes > 59) {
+      this.#duration.hours++
+      this.#duration.minutes = 0
+    }
+    this.#displayTimerElement.innerText = FORMATTER(this.#duration)
   }
 }
